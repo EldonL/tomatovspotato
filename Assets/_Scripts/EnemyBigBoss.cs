03438407dcf43dbd1e0d83442059c900f4233171
unknown <eldonlin24@gmail.com>
2023-06-13 01:53:40 -0700

    TOM-20 using DOTWEEN when level 2 and level 3 occurs. Lerp the cannon enemy and big boss into an endpoint so they don't suddenly appear

--- Assets/_Scripts/EnemyBigBoss.cs
+++ Assets/_Scripts/EnemyBigBoss.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class EnemyBigBoss : EnemyBase
 {
+    [SerializeField] private Transform endPoint;
     protected override void CollideWithPlayer()
     {
-        
+
+        //nothing yet
     }
 
     protected override IEnumerator EnabledEnemy()
     {
+        transform.DOMoveY(endPoint.position.y, 2f);
         yield break;
     }
 }
--- Assets/_Scripts/EnemyCannon.cs
+++ Assets/_Scripts/EnemyCannon.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class EnemyCannon : EnemyBase
 {
     [SerializeField] private Transform bombTransform;
     public WaitForSeconds shootBombSeconds = new WaitForSeconds(2.0f);
+    [SerializeField] private Transform endPoint;
+    [SerializeField] private bool allowMoveAtStart = false;
     protected override void CollideWithPlayer()
     {
 
@@ -14,6 +16,8 @@ public class EnemyCannon : EnemyBase
 
     protected override IEnumerator EnabledEnemy()
     {
+        if(allowMoveAtStart)
+            transform.DOMoveY(endPoint.position.y, 1f);
         yield return shootBombSeconds;
         while (true)
         {
