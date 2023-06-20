9fb6796739efe0423b6e24151df1a2e41ee37291
unknown <eldonlin24@gmail.com>
2023-06-19 21:20:40 -0700

    TOM-25 allow loading scene to go into lobby scene by adding  OnJoinedLobby and OnConnectToMaster callback in ConnectToServer script

--- Assets/_Scripts/Server/ConnectToServer.cs
+++ Assets/_Scripts/Server/ConnectToServer.cs
@@ -2,11 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-
-public class ConnectToServer : MonoBehaviour
+using UnityEngine.SceneManagement;
+public class ConnectToServer : MonoBehaviourPunCallbacks
 {
     private void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        SceneManager.LoadScene("Lobby");
     }
 }
