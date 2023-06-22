956ea409670d83817a5ad4b64dc728645d34840a
unknown <eldonlin24@gmail.com>
2023-06-22 00:41:50 -0700

    TOM-31 spawn master player on left side and client on right side so they don't overlap

--- Assets/_Scripts/Server/SpawnPlayer.cs
+++ Assets/_Scripts/Server/SpawnPlayer.cs
@@ -5,8 +5,13 @@ using Photon.Pun;
 public class SpawnPlayer : MonoBehaviour
 {
     [SerializeField] private GameObject _playerPrefab;
+    [SerializeField] private Transform _masterSpawnTransform;
+    [SerializeField] private Transform _clientSpawnTransform;
     private void Awake()
     {
-        PhotonNetwork.Instantiate(_playerPrefab.name, transform.position, transform.rotation);
+        if (PhotonNetwork.IsMasterClient)
+            PhotonNetwork.Instantiate(_playerPrefab.name, _masterSpawnTransform.position, _masterSpawnTransform.rotation);
+        else
+            PhotonNetwork.Instantiate(_playerPrefab.name, _clientSpawnTransform.position, _masterSpawnTransform.rotation);
     }
 }
