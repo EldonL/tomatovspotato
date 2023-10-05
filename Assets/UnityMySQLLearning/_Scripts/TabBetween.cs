c2a70ac3bc2cbc60adb3460ecee026575275afa8
unknown <eldonlin24@gmail.com>
2023-10-04 21:23:46 -0700

    TOM-58 allow user to press tab to go between username inputfield and password inputfield

---
+++ Assets/UnityMySQLLearning/_Scripts/TabBetween.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+namespace MySQLLearning
+{
+
+    [RequireComponent(typeof(TMP_InputField))]
+    public class TabBetween : MonoBehaviour
+    {
+
+        public TMP_InputField nextField;
+        TMP_InputField myField;
+        // Start is called before the first frame update
+        void Start()
+        {
+            if(nextField == null)
+            {
+                Destroy(this);
+                return;
+            }
+            myField = GetComponent<TMP_InputField>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(myField.isFocused && Input.GetKeyDown(KeyCode.Tab))
+            {
+                nextField.ActivateInputField();
+            }
+        }
+    }
+}
+
