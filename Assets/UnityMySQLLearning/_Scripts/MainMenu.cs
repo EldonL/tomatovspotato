f6bf8e28a50c0b1f95ee3f4470022f45dec376b1
unknown <eldonlin24@gmail.com>
2023-10-04 21:15:59 -0700

    TOM-58 Have references to play, login and register button so that we can check if they should be enabled or disabled based on in the user is logged in. If user is logged in then only the PlayGame button is enabled, otherwise only the register and login button is enabled

--- Assets/UnityMySQLLearning/_Scripts/MainMenu.cs
+++ Assets/UnityMySQLLearning/_Scripts/MainMenu.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.UI;
 namespace MySQLLearning
 {
+
+
     public class MainMenu : MonoBehaviour
     {
+        public Button registerButton;
+        public Button loginButton;
+        public Button playButton;
         public TextMeshProUGUI playerDisplay;
 
         private void Start()
@@ -15,6 +21,10 @@ namespace MySQLLearning
             {
                 playerDisplay.text = "Player: " + DBManager.username;
             }
+            registerButton.interactable = !DBManager.LoggedIn;
+            loginButton.interactable = !DBManager.LoggedIn;
+            playButton.interactable = DBManager.LoggedIn;
+
         }
 
         public void GoToRegister()
